929da91893cd1da4e5509821d6fa300c8b0d50b0
jabalka <jabalkada@gmail.com>
2023-11-08 17:51:14 +0000

    Buttons Fixed

--- controllers/edit.js
+++ controllers/edit.js
@@ -18,24 +18,26 @@ module.exports = {
             name: req.body.name,
             description: req.body.description,
             imageUrl: req.body.imageUrl,
-            difficulty: Number(req.body.difficulty)
+            difficulty: Number(req.body.difficulty),
         };
-        try{
+        try {
             await req.storage.edit(req.params.id, cube);
             res.redirect("/details/" + req.params.id);
-        } catch(err) {
-            // error occurs when trying to render the page as the cube can not be shown
-            if(err.name == 'ValidationError'){
+        } catch (err) {
+            if (err.name == "ValidationError") {
+                const cube = await req.storage.getById(req.params.id);
+                cube[`select${cube.difficulty}`] = true;
+                cube.name = req.body.name;
+                cube.description = req.body.description;
+                cube.imageUrl = req.body.imageUrl;
+                cube.difficulty = Number(req.body.difficulty);
                 const ctx = {
-                    title: 'Edit Cube',
-                    error: 'All fields are required! Image URL must be a valid URL',
-                    cube
+                    title: "Edit Cube",
+                    error: "All fields are required! Image URL must be a valid URL",
+                    cube,
                 };
-                res.render('edit', {... ctx, id: req.params.id});
-                // return res.redirect('edit/'+ req.params.id);
-            };
+                return res.render("edit", ctx);
+            }
         }
-        //         const cubeId = req.params.cubeId
-
     },
 };
