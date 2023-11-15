335558976ea6a1e9eaadba54f868cc749dbec557
jabalka <jabalkada@gmail.com>
2023-11-15 11:01:38 +0000

    Registration & Login implemented

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 router.get('/', (req, res) => res.redirect('/products'));
 
 router.get('/about', (req, res) => {
-    res.render('about', {title: 'About Page'});
+    res.render('about', {title: 'About'});
 });
 
 router.all('*', (req, res) => {
-    res.render('404', {title: 'Not Fount Page'});
+    res.render('404', {title: 'Not Fount'});
 });
 
 module.exports = router;
