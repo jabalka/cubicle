a582229400c0b33631535cb020da82bb21888f3f
jabalka <jabalkada@gmail.com>
2023-11-15 11:18:11 +0000

    implemented userSession via JWT

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,14 +2,17 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const userService = require("../services/user");
+const { COOKIE_NAME, TOKEN_SECRET } = require("../config");
 
 module.exports = () => (req, res, next) => {
     req.auth = {
         register,
         login,
     };
-
-    next();
+    
+    if(readToken(req)){
+        next();
+    }
 
     async function register({username, password, repeatPassword}){
         if(username == '' || password == '' || repeatPassword == ''){
@@ -41,9 +44,24 @@ module.exports = () => (req, res, next) => {
 
     function createToken(user){
         const userViewModel = {_id: user._id, username: user.username};
-        const token = jwt.sign(userViewModel, 'my very secure secret');
-        res.cookie("SESSION_DATA", token, { httpOnly: true });
+        const token = jwt.sign(userViewModel, TOKEN_SECRET);
+        res.cookie(COOKIE_NAME, token, { httpOnly: true });
 
         return userViewModel;
+    }
+
+    function readToken(req){
+        const token = req.cookies[COOKIE_NAME];
+        if(token){
+            try{
+                const userData = jwt.verify(token, TOKEN_SECRET);
+                req.user = userData;
+            } catch (err){
+                res.clearCookie(COOKIE_NAME);
+                res.redirect('/auth/login');
+                return false;
+            }
+        }
+        return true;
     }
 };
